36d105f5a6818b43a55a0848be225b077581ffdd
Fuhad Ihthisham <ihthisham.v@gmail.com>
2021-09-28 13:05:56 +0530

    Contact me section updated

---
+++ forms/form.js
@@ -0,0 +1,41 @@
+var reg = $("#contact-form");
+
+reg.validate({
+  rules: {
+    Username: {
+      required: true,
+      minlength: 4,
+    },
+    email: {
+      required: true,
+    },
+    subject: {
+      required: true,
+    },
+    message: {
+      required: true,
+    },
+  },
+
+  messages: {
+    username: {
+      required: "This field is required.",
+    },
+  },
+
+  submitHandler: function (form) {
+    $.ajax({
+      url: "https://script.google.com/macros/s/AKfycbw1rqAfKqqOwDAiatTB3FLKxCbaASddAdbgyGlzeYj4LoINDKwa/exec",
+      data: $("#contact-form").serialize(),
+      method: "post",
+      success: function (response) {
+        alert("Form submitted successfully");
+        window.location.reload();
+        // window.location.href = "index.html";
+      },
+      error: function (err) {
+        alert("Something Error");
+      },
+    });
+  },
+});
